b0a4626104422664308a5d3f03524b4282c71a4b
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-03-28 19:44:04 +0100

    Add project files.

---
+++ PokedexMVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokedexMVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
