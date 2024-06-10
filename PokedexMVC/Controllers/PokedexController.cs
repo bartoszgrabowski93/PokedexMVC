79b87cbd4d405548a35acfc810e8149d2381eca2
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-06-10 18:54:02 +0200

    Changes after pc format

--- PokedexMVC/Controllers/PokedexController.cs
+++ PokedexMVC/Controllers/PokedexController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
+
+
 namespace PokedexMVC.Web.Controllers
 {
     public class PokedexController : Controller
@@ -16,8 +18,9 @@ namespace PokedexMVC.Web.Controllers
             // serwis przygotowuje dane 
             // serwis zwraca dane do kontrollera w odpowiednim formacie
 
-            var model = pokemonService.GetAllPokemonForList();
-            return View(model);
+            /* var model = pokemonService.GetAllPokemonForList();
+            return View(model); */
+            return View();
         }
 
         // GET: PokedexController/Details/5
@@ -34,18 +37,18 @@ namespace PokedexMVC.Web.Controllers
         }
 
         // POST: PokedexController/Create
-        [HttpPost]        
-        public IActionResult AddPokemon(PokemonModel model)
+        /*[HttpPost]        
+         public IActionResult AddPokemon(PokemonModel model)
         {
             var id = pokemonService.AddPokemon(model);
             return View();
-        }
+        } */
         [HttpGet]
         public IActionResult AddPokemonDescription(int pokemonId)
         {
             return View();
         }
-        [HttpPost]
+        /* [HttpPost]
         public IActionResult AddPokemonDescription(PokemonDescriptionModel model)
         {
             var id = pokemonService.AddPokemonDescription(model);
@@ -56,7 +59,7 @@ namespace PokedexMVC.Web.Controllers
         {
             var pokemonDetails = pokemonService.GetPokemonDetailsById(pokemondId);
             return View(pokemonDetails);
-        }
+        } */
 
     }
 }
