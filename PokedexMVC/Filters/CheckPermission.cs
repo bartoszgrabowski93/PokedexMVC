380f9a635e75295eed5f914d2b773341d09d0b6f
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-10-24 18:59:15 +0200

    Adding custom fillters

---
+++ PokedexMVC/Filters/CheckPermission.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Security.Claims;
+
+namespace PokedexMVC.Web.Filters
+{
+    public class CheckPermission : Attribute, IAuthorizationFilter
+    {
+        private readonly string _permission;
+
+        public CheckPermission(string permission)
+        {
+            _permission = permission;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            bool isAuthorized = CheckUserPermission(context.HttpContext.User, _permission);
+        }
+
+        private bool CheckUserPermission(ClaimsPrincipal user, string permission)
+        {
+            return permission == "Read";
+        }
+    }
+}
