179ef3f304a2c2a3b2a2435809acee69bd4c8ef0
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-11-02 17:45:29 +0100

    Adding Authorization by generating JwtSecurityTokens

--- PokedexApi/Controllers/LoginController.cs
+++ PokedexApi/Controllers/LoginController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace PokedexApi.Controllers
 {
@@ -8,6 +12,13 @@ namespace PokedexApi.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _config;
+        public LoginController(IConfiguration config,SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _config = config;
+        }
         [AllowAnonymous]
         [HttpPost]
         public IActionResult Login([FromBody] UserModel loginModel)
@@ -23,14 +34,20 @@ namespace PokedexApi.Controllers
             return response;
         }
 
-        private object GenerateJsonWebToken(UserModel loginModel)
+        private string GenerateJsonWebToken(UserModel loginModel)
         {
-            throw new NotImplementedException();
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Issuer"], null, expires: DateTime.Now.AddMinutes(120), signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
-        private bool AuthenticateUser(object loginModel)
+        private bool AuthenticateUser(UserModel loginModel)
         {
-            throw new NotImplementedException();
+            var result = _signInManager.PasswordSignInAsync(loginModel.Email, loginModel.Password, true, lockoutOnFailure: false).Result;
+
+            return result.Succeeded;
         }
     }
 }
--- PokedexApi/UserModel.cs
+++ PokedexApi/UserModel.cs
@@ -1,6 +1,10 @@
-﻿namespace PokedexApi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PokedexApi
 {
     public class UserModel
     {
+        public string Email { get; set; }     
+        public string Password { get; set; }
     }
 }
