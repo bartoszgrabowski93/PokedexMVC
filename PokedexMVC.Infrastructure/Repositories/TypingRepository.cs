2380acbe96ec17a5db7e097d5dc75c69cd2f4f32
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-12-12 11:54:02 +0100

    Implementing new methods into TypingRepo

--- PokedexMVC.Infrastructure/Repositories/TypingRepository.cs
+++ PokedexMVC.Infrastructure/Repositories/TypingRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace PokedexMVC.Infrastructure.Repositories
 {
-    public class TypingRepository : ITypingRepository
+    public class TypingRepository : IBasicTypingRepository, ICombinationTypingRepository
     {
         private readonly Context _context;
         public TypingRepository(Context context)
@@ -16,36 +16,87 @@ namespace PokedexMVC.Infrastructure.Repositories
             _context = context;
         }
 
-        public int AddTyping(BasicType type)
+        public int AddBasicTyping(BasicType type)
         {
             _context.BasicTypes.Add(type);
             _context.SaveChanges();
             return type.Id;
         }
-        public void DeleteTyping(int typingId)
+
+        public int AddCombinationTyping(TypeCombination typeCombination)
+        {
+            _context.TypeCombinations.Add(typeCombination);
+            _context.SaveChanges();
+            return typeCombination.Id;
+        }
+
+        public void AssignCombinationTypeToPokemon(Pokemon pokemon, TypeCombination typeCombination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBasicTyping(int typingId)
         {
             var typingToDelete = _context.BasicTypes.Find(typingId);
             if (typingToDelete != null)
             {
                 _context.BasicTypes.Remove(typingToDelete);
                 _context.SaveChanges();
-            }else
+            }
+            else
             {
                 Console.WriteLine($"Could not find Type with id : {typingId}.");
             }
         }
 
-        public IQueryable<BasicType> GetAllTypings()
+        public void DeleteCombinationTyping(int combinationToDeleteId)
         {
-            IQueryable<BasicType> typings = _context.BasicTypes;
-            return typings; 
+            var comboTypeToDelete = _context.TypeCombinations.Find(combinationToDeleteId);
+            if (comboTypeToDelete != null)
+            {
+                _context.TypeCombinations.Remove(comboTypeToDelete);
+                _context.SaveChanges();
+            }
+            else
+            {
+                Console.WriteLine($"Could not find Type with id : {combinationToDeleteId}.");
+            }
+        }
+        
+
+        public void EditBasicTyping(int typingToEditId, BasicType typeNewData)
+        {
+            var basicTypingToEdit = _context.BasicTypes.Find(typingToEditId);
+            if (basicTypingToEdit != null)
+            {
+                basicTypingToEdit.Name = typeNewData.Name;
+
+            }
+            else
+            {
+                Console.WriteLine($"Could not find Type with id : {typingToEditId}.");
+            }
         }
 
-        public void SetTyping(Pokemon pokemon, TypeCombination typeCombo)
+        public IQueryable<BasicType> GetAllBasicTypings()
         {
             throw new NotImplementedException();
         }
 
-            
+        public IQueryable<TypeCombination> GetAllCombinationTyping()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BasicType GetBasicTypeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TypeCombination GetTypeCombinationById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
-}
+
+        
