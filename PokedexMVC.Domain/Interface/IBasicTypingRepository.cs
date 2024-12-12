8d42c4b9f7b467aeb2a5cd724ed854e5eb61568d
Bartosz Grabowski <bartosz.g.grabowski@gmail.com>
2024-12-12 11:53:44 +0100

    Spliting interface into two for easier later implementation

---
+++ PokedexMVC.Domain/Interface/IBasicTypingRepository.cs
@@ -0,0 +1,22 @@
+﻿using PokedexMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexMVC.Domain.Interface
+{
+    public interface IBasicTypingRepository
+    {
+        int AddBasicTyping(BasicType type);
+
+        void DeleteBasicTyping(int typingId);
+        void EditBasicTyping(int typingToEditId, BasicType typeNewData);
+
+        IQueryable<BasicType> GetAllBasicTypings();
+        BasicType GetBasicTypeById(int id);
+       
+       
+    }
+}
---
+++ PokedexMVC.Domain/Interface/ICombinationTypingRepository.cs
@@ -0,0 +1,19 @@
+﻿using PokedexMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokedexMVC.Domain.Interface
+{
+    public interface ICombinationTypingRepository
+    {
+        int AddCombinationTyping(TypeCombination typeCombination);
+        void DeleteCombinationTyping(int combinationToDeleteId);
+        IQueryable<TypeCombination> GetAllCombinationTyping();
+        TypeCombination GetTypeCombinationById(int id);
+        void AssignCombinationTypeToPokemon(Pokemon pokemon, TypeCombination typeCombination);
+       
+    }
+}
